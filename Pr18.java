0bbd3e80e860aa0a9280bf48c07c2853ce3c57f7
Khevna134 <khevnaraval134@gmail.com>
2026-04-18 22:52:26 +0530

    Pr18.java

---
+++ Pr18.java
@@ -0,0 +1,42 @@
+abstract class Vehicle {
+    abstract String fuelType();
+    abstract int noOfWheels();
+}
+
+class Car extends Vehicle {
+    String fuelType() {
+        return "Petrol/Diesel";
+    }
+
+    int noOfWheels() {
+        return 4;
+    }
+}
+
+class Bike extends Vehicle {
+    String fuelType() {
+        return "Petrol";
+    }
+
+    int noOfWheels() {
+        return 2;
+    }
+}
+
+public class pr18 {
+    public static void main(String[] args) {
+        Vehicle c = new Car();
+        Vehicle b = new Bike();
+
+        System.out.println("Car Fuel: " + c.fuelType());
+        System.out.println("Car Wheels: " + c.noOfWheels());
+
+        System.out.println("Bike Fuel: " + b.fuelType());
+        System.out.println("Bike Wheels: " + b.noOfWheels());
+    }
+}
+//output
+//Car Fuel: Petrol/Diesel
+//Car Wheels: 4
+//Bike Fuel: Petrol
+//Bike Wheels: 2
