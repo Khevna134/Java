578e351e972d278f1379d7b0856f401e81622150
Khevna134 <khevnaraval134@gmail.com>
2026-04-29 20:32:31 +0530

    Pr33.java

---
+++ Pr33.java
@@ -0,0 +1,37 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.FontPosture;
+import javafx.scene.paint.Color;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import java.util.Random;
+
+public class pr33 extends Application {
+    public void start(Stage stage) {
+        VBox vbox = new VBox(15);
+        vbox.setAlignment(Pos.CENTER);
+
+        Random rand = new Random();
+
+        for (int i = 1; i <= 5; i++) {
+            Text text = new Text("Text " + i);
+            text.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 22));
+            text.setFill(Color.color(rand.nextDouble(), rand.nextDouble(), rand.nextDouble()));
+            text.setOpacity(0.3 + (rand.nextDouble() * 0.7));
+            vbox.getChildren().add(text);
+        }
+
+        Scene scene = new Scene(vbox, 300, 250);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX Text Styling");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
